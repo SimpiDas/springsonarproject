c577eae3143880ced2b005477a58510356b9ce19
Roy Anushua (BGSW/PJ-ETA-B) <ryy1cob@bosch.com>
2024-12-17 10:34:30 +0530

    first commit

---
+++ src/test/java/com/basics/SpringbootsonardataprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsonardataprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
